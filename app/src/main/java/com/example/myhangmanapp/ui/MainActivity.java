2a86787cd8354f0f6d13ea4d58ca3d36ffd4b1ec
Andi365 <Anders1998@live.dk>
2019-09-30 22:54:26 +0200

    added support for changing screens and sending the name along Fixed Manifest with main activity (oops)

--- app/src/main/java/com/example/myhangmanapp/ui/MainActivity.java
+++ app/src/main/java/com/example/myhangmanapp/ui/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.myhangmanapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
 
 import com.example.myhangmanapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    private EditText nameField;
+    private Button startButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = findViewById(R.id.nameContainer);
+        startButton = findViewById(R.id.nextPage);
+
+        startButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View isClicked) {
+        String name = nameField.getText().toString();
+
+        if(startButton == isClicked) {
+            Resources resources = getResources();
+            String key = resources.getString(R.string.name_key);
+            Intent intent = new Intent(MainActivity.this,GameActivity.class);
+            intent.putExtra(key, name);
+            startActivity(intent);
+        }
     }
 }
